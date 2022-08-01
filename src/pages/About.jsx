86363a586da0f5f31bbbe92a449afdc7af49cfe5
Omar Ragheb <ahmed.raph29@gmail.com>
2022-08-02 02:02:05 +0300

    Add about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,47 @@
-const About = () => (
-  <div className="flex flex-col items-center justify-center h-screen bg-gray-300" id="about">
-    <h1 className="text-3xl font-bold text-center">About</h1>
-    <p className="text-center">
-      This is a simple example of a React component.
-    </p>
-  </div>
-);
+/* eslint-disable max-len */
+import AOS from 'aos';
+
+const About = () => {
+  AOS.init({
+    duration: 1500,
+    startEvent: 'load',
+    delay: 300,
+  });
+
+  return (
+    <div className=" h-auto bg-gray-200" id="about">
+      <div className="flex flex-col items-center justify-start" data-aos="fade-right">
+        <h1 className="text-3xl font-bold text-center heading">About</h1>
+        <div className="description container bg-[#f5f5f5] rounded-2xl p-4 my-6 w-[80%] text-[1rem] md:text-[1.2rem]">
+          <p className="text-center w-[80%] my-4 mx-auto">
+            Hello, my name is
+            {' '}
+            <span>Lucas</span>
+            , I’ve finished
+            {' '}
+            <span>full-stack development</span>
+            {' '}
+            course at
+            Microverse, an online school where I’m not only learning code working on real-world projects but also
+            learning how to work in teams from different parts of the world doing pair programming using JavaScript,
+            React, Redux, Ruby and Rails, and I&apos;m even a
+            {' '}
+            <span>mentor</span>
+            {' '}
+            there.
+          </p>
+          <p className="text-center w-[80%] my-4 mx-auto">
+            {' '}
+            I love solving code algorithms because before I was like a puzzle/strategic gamer, then coding became the
+            perfect replacement for that.
+          </p>
+          <p className="text-center w-[80%] my-4 mx-auto">
+            In my free time, I like to watch movies, play video games, and try to improve my piano skills.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
