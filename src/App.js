fda551309c8634b2d7a696616b87978c9c036d05
Omar Ragheb <ahmed.raph29@gmail.com>
2022-08-02 02:01:21 +0300

    Add separator

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 import Showcase from './pages/Showcase';
+import Separator from './components/Separator';
+import Skills from './pages/Skills';
 
 function App() {
   return (
     <>
       <Navbar />
       <Showcase />
-      <div className="h-[100px] md:h-[200px] top">
-        <svg className="fill-gray-300 w-[100vw] h-[100%]" viewBox="0 0 500 150" preserveAspectRatio="none">
-          <path
-            d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
-          />
-        </svg>
-      </div>
+      <Separator />
       <About />
+      <Skills />
       <Projects />
       <Contact />
     </>
