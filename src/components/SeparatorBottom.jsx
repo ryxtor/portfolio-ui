6afd4b6baa4b6a0c9d398747107883cf1efcbdbf
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-08-02 19:59:14 -0300

    Create separator bottom

---
+++ src/components/SeparatorBottom.jsx
@@ -0,0 +1,9 @@
+const SeparatorBottom = () => (
+  <div className="h-[100px] md:h-[200px]">
+    <svg viewBox="0 0 500 150" preserveAspectRatio="none" className="fill-gray-200 w-[100vw] h-[100%]">
+      <path d="M0.00,49.98 C149.99,150.00 271.49,-49.98 500.00,49.98 L500.00,0.00 L0.00,0.00 Z" />
+    </svg>
+  </div>
+);
+
+export default SeparatorBottom;
