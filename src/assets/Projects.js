7ccb3d48f8cde48c1e5f080ded7fbf855ea776df
ryxtor <ryxtor@outlook.com>
2023-11-04 20:25:49 -0300

    Update projects

--- src/assets/Projects.js
+++ src/assets/Projects.js
@@ -1,25 +1,150 @@
 import bookdev from '../images/bookdev.png';
+import budgetApp from '../images/budget_app.png';
+import recipeApp from '../images/recipe_app.png';
+import spaceTravelers from '../images/space_travelers.png';
+import tvShows from '../images/tv_shows.png';
+import leaderboard from '../images/leaderboard.png';
 
 const projects = [
   {
     id: 1,
-    name: 'Project 1',
-    description: 'This is a project',
+    name: 'Book Dev',
+    description:
+      'This is a web page that let you make an appointment with a developer. This app uses API calls to its back-end to get the data regarding users, developers, and skills.',
     image: bookdev,
-    github_link: 'https://github.com',
-    live_demo: 'https://github.com',
-    skills: [{
-      id: 1,
-      name: 'HTML',
-    },
-    {
-      id: 2,
-      name: 'CSS',
-    },
-    {
-      id: 3,
-      name: 'JavaScript',
-    }],
+    github_link: 'https://github.com/awaisanwar544/final-capstone-ui',
+    live_demo: null,
+    skills: [
+      {
+        id: 1,
+        name: 'Rails',
+      },
+      {
+        id: 2,
+        name: 'React',
+      },
+      {
+        id: 3,
+        name: 'Tailwind',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Budget App',
+    description:
+      'This is a Budget web mobile app where you can register, login, create categories, add transactions and see the total amount for each category.',
+    image: budgetApp,
+    github_link: 'https://github.com/ryxtor/budget-app',
+    live_demo: null,
+    skills: [
+      {
+        id: 1,
+        name: 'Rails',
+      },
+      {
+        id: 2,
+        name: 'PostgreSQL',
+      },
+      {
+        id: 3,
+        name: 'Bootstrap',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Recipe App',
+    description:
+      'This is an app to store your recipes and make them public! After login, you can create new recipes with the food you added to your favorite foods. You can also store the ingredients you currently have in an inventory and compare the recipe you want to follow with your inventory to get a custom shopping list with the items you need!',
+    image: recipeApp,
+    github_link: 'https://github.com/ryxtor/recipe-app',
+    live_demo: null,
+    skills: [
+      {
+        id: 1,
+        name: 'Rails',
+      },
+      {
+        id: 2,
+        name: 'PostgreSQL',
+      },
+      {
+        id: 3,
+        name: 'Bootstrap',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Space Travelers Hub',
+    description:
+      '"Space Travelers Hub" is a website that work with real live data from the SpaceX API. The users can book rockets and join selected space missions.',
+    image: spaceTravelers,
+    github_link: 'https://github.com/yersel500/space-travelers-hub',
+    live_demo: 'https://loquacious-axolotl-f182b5.netlify.app/',
+    skills: [
+      {
+        id: 1,
+        name: 'React',
+      },
+      {
+        id: 2,
+        name: 'Redux',
+      },
+      {
+        id: 3,
+        name: 'JavaScript',
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: 'TV Shows',
+    description:
+      'In this project we made a Tv shows webpage where you can like or comment the different shows.',
+    image: tvShows,
+    github_link:
+      'https://github.com/ryxtor/JS-group-capstone-project-Lucas-Omar',
+    live_demo:
+      'https://ryxtor.github.io/JS-group-capstone-project-Lucas-Omar/dist/',
+    skills: [
+      {
+        id: 1,
+        name: 'JavaScript',
+      },
+      {
+        id: 2,
+        name: 'HTML',
+      },
+      {
+        id: 3,
+        name: 'CSS',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'Leaderboard',
+    description:
+      'This is a tool for fun purposes, you can add a new score to the leaderboard using API for storage. The design is based on the Elden Ring video game.',
+    image: leaderboard,
+    github_link: 'https://github.com/ryxtor/Leaderboard',
+    live_demo: 'https://ryxtor.github.io/Leaderboard/dist/',
+    skills: [
+      {
+        id: 1,
+        name: 'JavaScript',
+      },
+      {
+        id: 2,
+        name: 'HTML',
+      },
+      {
+        id: 3,
+        name: 'CSS',
+      },
+    ],
   },
 ];
 
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -75,7 +75,9 @@ const Project = ({ project }) => {
     const element = document.getElementById(card);
     const ELEMENTS_SPAN = [];
 
-    if (!ELEMENTS_SPAN[0]) { ELEMENTS_SPAN[0] = document.getElementById(`span-${project.id}`); }
+    if (!ELEMENTS_SPAN[0]) {
+      ELEMENTS_SPAN[0] = document.getElementById(`span-${project.id}`);
+    }
 
     element.addEventListener('mouseover', (e) => {
       ELEMENTS_SPAN[0].style.left = `${e.pageX - element.offsetLeft}px`;
@@ -102,48 +104,97 @@ const Project = ({ project }) => {
 
   return (
     <div
-      className="flex justify-center flex-wrap w-auto xl:w-[26%] transition duration-500"
+      className="flex select-none justify-center flex-wrap w-auto xl:w-[26%] transition duration-500"
       data-aos="fade-up-left"
       data-aos-easing="ease-out-back"
       data-aos-duration="1200"
     >
-      <div
-        className="transition duration-500 shadow-xl overflow-hidden rounded-lg w-[300px] m-[10px] bg-gray-100 border dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300"
-      >
-        <div id={`card-${project.id}`} className="HOVER relative inline-block overflow-hidden">
-          <span id={`span-${project.id}`} className="absolute block w-0 h-0 rounded-full bg-black opacity-50" />
+      <div className="transition duration-500 shadow-xl overflow-hidden rounded-lg w-[300px] m-[10px] bg-gray-100 border dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300">
+        <div
+          id={`card-${project.id}`}
+          className="HOVER relative inline-block overflow-hidden"
+        >
+          <span
+            id={`span-${project.id}`}
+            className="absolute block w-0 h-0 rounded-full bg-black opacity-50"
+          />
           <div className="icon-hover flex gap-10 absolute top-[50%] left-[50%] m-0 p-0 opacity-0">
             <a href={project.live_demo} target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faEye} className="hover:text-purple-800 drop-shadow transition" title="Live Demo" size="2x" color="white" />
+              <FontAwesomeIcon
+                icon={faEye}
+                className={`${project.live_demo ? 'hover:text-purple-800' : ''} drop-shadow transition`}
+                title="Live Demo"
+                size="2x"
+                color="white"
+              />
             </a>
             <a href={project.github_link} target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faGithub} className="hover:text-purple-800 drop-shadow transition" title="GitHub Repository" size="2x" color="white" />
+              <FontAwesomeIcon
+                icon={faGithub}
+                className={`${project.github_link ? 'hover:text-purple-800' : ''} drop-shadow transition`}
+                title="GitHub Repository"
+                size="2x"
+                color="white"
+              />
             </a>
           </div>
           <div className="skill-hover flex items-center absolute bottom-5 w-[100%] opacity-0 mx-auto">
             {project.skills.map((skill) => (
-              <img key={skill.id} src={icons[skill.name]} alt={skill.name} title={skill.name} className="h-7 drop-shadow w-[100%] object-contain" />
+              <img
+                key={skill.id}
+                src={icons[skill.name]}
+                alt={skill.name}
+                title={skill.name}
+                className="h-7 drop-shadow w-[100%] object-contain"
+              />
             ))}
           </div>
 
-          <img src={project.image} alt={project.name} className="w-[100%] h-[200px] object-cover" />
+          <img
+            src={project.image}
+            alt={project.name}
+            className="w-[100%] h-[200px] object-cover"
+          />
         </div>
         <div className="flex flex-col items-start p-[20px] min-h-[200px]">
-          <span className="text-gray-200 mx-auto mb-4 shadow-md p-2 rounded-[50px] font-Raleway" style={{ background: colors[project.id] }}>{project.name}</span>
+          <span
+            className="text-gray-200 mx-auto mb-4 shadow-md p-2 rounded-[50px] font-Raleway"
+            style={{ background: colors[project.id] }}
+          >
+            {project.name}
+          </span>
           <p className="font-Raleway m-0 text-[0.9rem]">
-            { project.description.length > 150
-              ? truncate(project.description) : project.description }
+            {project.description.length > 150
+              ? truncate(project.description)
+              : project.description}
           </p>
         </div>
         <div className="flex justify-center gap-5 mb-3">
-          <button type="button" onClick={openModal} className="transition border border-purple-700 w-[85px] shadow-md bg-purple-800 hover:bg-purple-600 dark:hover:bg-purple-700 text-gray-200 font-Raleway font-bold py-2 px-4 rounded-lg">
+          <button
+            type="button"
+            onClick={openModal}
+            className="transition border border-purple-700 w-[85px] shadow-md bg-purple-800 hover:bg-purple-600 dark:hover:bg-purple-700 text-gray-200 font-Raleway font-bold py-2 px-4 rounded-lg"
+          >
             Details
           </button>
-          <button type="button" className="transition border border-purple-700 w-[85px] shadow-md bg-purple-800 hover:bg-purple-600 dark:hover:bg-purple-700 text-gray-200 font-Raleway font-bold py-2 px-4 rounded-lg">
-            <a href={project.github_link} target="_blank" rel="noreferrer">
+          {project.github_link ? (
+            <a
+              className="transition border border-purple-700 w-[85px] shadow-md bg-purple-800 hover:bg-purple-600 dark:hover:bg-purple-700 text-gray-200 font-Raleway font-bold py-2 px-4 rounded-lg"
+              href={project.github_link}
+              target="_blank"
+              rel="noreferrer"
+            >
               Source
             </a>
-          </button>
+          ) : (
+            <button
+              type="button"
+              className="transition border border-gray-500 w-[85px] shadow-md bg-gray-500 text-gray-200 font-Raleway font-bold py-2 px-4 rounded-lg"
+              disabled
+            >
+              Source
+            </button>
+          )}
         </div>
       </div>
       <Modal
@@ -175,11 +226,16 @@ const Project = ({ project }) => {
       >
         <div className="flex flex-col items-start h-[100%]">
           <div className="md:flex">
-
-            <img src={project.image} alt={project.name} className="w-[100%] md:w-[50%] lg:w-[60%] xl:w-[70%] h-[250px] object-cover md:object-contain md:h-auto self-center" />
+            <img
+              src={project.image}
+              alt={project.name}
+              className="w-[100%] md:w-[50%] lg:w-[60%] xl:w-[70%] h-[250px] object-cover md:object-contain md:h-auto self-center"
+            />
             <div className="flex h-[100%] w-[100%] items-center flex-col">
               <div className="flex flex-col items-center">
-                <h3 className="font-Finger-Paint text-purple-600 drop-shadow-lg text-xl py-3">{project.name}</h3>
+                <h3 className="font-Finger-Paint text-purple-600 drop-shadow-lg text-xl py-3">
+                  {project.name}
+                </h3>
                 <p className="font-Raleway dark:text-gray-300 self-center mx-3 text-[0.9rem] text-center">
                   {project.description}
                 </p>
@@ -187,29 +243,69 @@ const Project = ({ project }) => {
             </div>
           </div>
           <div className="flex flex-col md:flex-row justify-center items-center w-[100%] md:dark:bg-gray-800 md:bg-gray-300 ">
-
             <div className="flex items-center justify-around w-[100%] lg:w-[60%] xl:w-[70%] py-3">
               {project.skills.map((skill) => (
-                <div className="group flex flex-col flex-grow w-[33%] items-center justify-center gap-3 " key={skill.name}>
-                  <img key={skill.id} src={icons[skill.name]} alt={skill.name} title={skill.name} className="h-7 drop-shadow w-[100%] object-contain" />
-                  <p className="transition duration-300 group-hover:text-purple-700 text-center text-sm font-Raleway font-semibold text-gray-700 dark:text-gray-300">{skill.name}</p>
+                <div
+                  className="group flex flex-col flex-grow w-[33%] items-center justify-center gap-3 "
+                  key={skill.name}
+                >
+                  <img
+                    key={skill.id}
+                    src={icons[skill.name]}
+                    alt={skill.name}
+                    title={skill.name}
+                    className="h-7 drop-shadow w-[100%] object-contain"
+                  />
+                  <p className="transition duration-300 group-hover:text-purple-700 text-center text-sm font-Raleway font-semibold text-gray-700 dark:text-gray-300">
+                    {skill.name}
+                  </p>
                 </div>
               ))}
             </div>
             <div className="flex mt-3 justify-around mb-3 w-[100%] lg:w-[40%] xl:w-[30%]">
-              <button type="button" className="transition bg-purple-800 hover:bg-purple-900 text-gray-200 font-Raleway font-bold py-2 px-4 rounded-lg w-[112px]">
-                <a href={project.live_demo} target="_blank" rel="noreferrer">
+              {project.live_demo ? (
+                <a
+                  className="transition bg-purple-800 hover:bg-purple-900 text-gray-200 font-Raleway font-bold py-2 px-4 rounded-lg w-[112px]"
+                  href={project.live_demo}
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   Live Demo
                 </a>
-              </button>
-              <button type="button" className="transition bg-purple-800 hover:bg-purple-900 text-gray-200 font-Raleway font-bold py-2 px-4 rounded-lg w-[112px]">
-                <a href={project.github_link} target="_blank" rel="noreferrer">
+              ) : (
+                <button
+                  type="button"
+                  className="transition bg-gray-500 text-gray-200 font-Raleway font-bold py-2 px-4 rounded-lg w-[112px]"
+                  disabled
+                >
+                  Live Demo
+                </button>
+              )}
+              {project.github_link ? (
+                <a
+                  className="transition border border-purple-700 w-[85px] shadow-md bg-purple-800 hover:bg-purple-600 dark:hover:bg-purple-700 text-gray-200 font-Raleway font-bold py-2 px-4 rounded-lg"
+                  href={project.github_link}
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   Source
                 </a>
-              </button>
+              ) : (
+                <button
+                  type="button"
+                  className="transition border border-gray-500 w-[85px] shadow-md bg-gray-500 text-gray-200 font-Raleway font-bold py-2 px-4 rounded-lg"
+                  disabled
+                >
+                  Source
+                </button>
+              )}
             </div>
           </div>
-          <button type="button" onClick={closeModal} className="absolute top-[255px] md:top-0 right-0 transition ease-out duration-300 bg-gray-200 bg-opacity-0 md:hover:bg-gray-300 dark:md:hover:bg-gray-800 text-purple-700 drop-shadow font-Finger-Paint font-bold py-2 px-4 rounded-lg">
+          <button
+            type="button"
+            onClick={closeModal}
+            className="absolute top-[255px] md:top-0 right-0 transition ease-out duration-300 bg-gray-200 bg-opacity-0 md:hover:bg-gray-300 dark:md:hover:bg-gray-800 text-purple-700 drop-shadow font-Finger-Paint font-bold py-2 px-4 rounded-lg"
+          >
             X
           </button>
         </div>
@@ -223,13 +319,15 @@ Project.propTypes = {
     name: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
     description: PropTypes.string.isRequired,
-    github_link: PropTypes.string.isRequired,
-    live_demo: PropTypes.string.isRequired,
+    github_link: PropTypes.string,
+    live_demo: PropTypes.string,
     image: PropTypes.string.isRequired,
-    skills: PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      id: PropTypes.number.isRequired,
-    })).isRequired,
+    skills: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
+      }),
+    ).isRequired,
   }).isRequired,
 };
 
