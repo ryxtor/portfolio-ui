4479789245901d68cd9a55bc4668b8cbacd3af7f
ryxtor <ryxtor@outlook.com>
2023-11-04 15:47:10 -0300

    Start changing projects

---
+++ src/assets/Projects.js
@@ -0,0 +1,33 @@
+import bookdev from '../images/bookdev.png';
+
+const projects = [
+  {
+    id: 1,
+    name: 'Project 1',
+    description: 'This is a project',
+    image: bookdev,
+    github_link: 'https://github.com',
+    live_demo: 'https://github.com',
+    skills: [{
+      id: 1,
+      name: 'HTML',
+    },
+    {
+      id: 2,
+      name: 'CSS',
+    },
+    {
+      id: 3,
+      name: 'JavaScript',
+    }],
+  },
+];
+
+export default projects;
+
+// name
+// description
+// image
+// github_link
+// live_demo
+// skills []
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,28 +1,21 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import Project from '../components/Project';
+import projects from '../assets/Projects';
 
-const Projects = () => {
-  const [projects, setProjects] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('https://portfolio-j0yt.onrender.com/v1/projects');
-      if (response.ok) {
-        const data = await response.json();
-        setProjects(data);
-      }
-    })();
-  }, []);
-
-  return (
-    <div className="transition duration-500 h-auto bg-gray-100 dark:bg-gray-800" id="projects">
-      <h1 className="transition duration-500 text-3xl font-bold text-center mb-8 after:bg-purple-700 dark:after:bg-purple-800 after:content-[''] after:block after:h-1 after:my-3 after:mx-auto after:w-32 dark:text-gray-300 font-Finger-Paint">Projects</h1>
-      <div className="flex flex-wrap items-center justify-center">
-        {projects.map((project) => (
-          <Project key={project.id} project={project} />
-        ))}
-      </div>
+const Projects = () => (
+  <div
+    className="transition duration-500 h-auto bg-gray-100 dark:bg-gray-800"
+    id="projects"
+  >
+    <h1 className="transition duration-500 text-3xl font-bold text-center mb-8 after:bg-purple-700 dark:after:bg-purple-800 after:content-[''] after:block after:h-1 after:my-3 after:mx-auto after:w-32 dark:text-gray-300 font-Finger-Paint">
+      Projects
+    </h1>
+    <div className="flex flex-wrap items-center justify-center">
+      {projects.map((project) => (
+        <Project key={project.id} project={project} />
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default Projects;
